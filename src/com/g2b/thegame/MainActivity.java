39026d681e9525de10736d458a8334917ccdcf22
Vladimir Zarypov <krre31@gmail.com>
2013-07-28 16:06:04 +0300

    добавлен экран игры и код навигации между экранами

--- src/com/g2b/thegame/MainActivity.java
+++ src/com/g2b/thegame/MainActivity.java
@@ -1,24 +1,40 @@
 package com.g2b.thegame;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
+
+    Button producerBtn;
+    Button gameBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.producer);
-//        setContentView(R.layout.activity_main);
-    }
+        setContentView(R.layout.main);
 
+        gameBtn = (Button) findViewById(R.id.game_button);
+        gameBtn.setOnClickListener(this);
+        producerBtn = (Button) findViewById(R.id.producer_button);
+        producerBtn.setOnClickListener(this);
+    }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.producer_button:
+                Intent producerIntent = new Intent(this, ProducerActivity.class);
+                startActivity(producerIntent);
+                break;
+            case R.id.game_button:
+                Intent gameIntent = new Intent(this, GameActivity.class);
+                startActivity(gameIntent);
+            default:
+                break;
+        }
     }
-    
 }
