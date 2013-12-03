4c601aae932c419e7945a6a8ecc9bf0601ae00fe
Vladimir Zarypov <krre31@gmail.com>
2013-12-03 12:35:23 +0200

    добавлен текст для раздела примечаний

--- cinema/src/com/g2b/cinema/actors/Description.java
+++ cinema/src/com/g2b/cinema/actors/Description.java
@@ -1,14 +1,21 @@
 package com.g2b.cinema.actors;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 
 public class Description extends Actor {
     private ShapeRenderer shapeRenderer;
+    private BitmapFont bitmapFont;
 
     public Description() {
         shapeRenderer = new ShapeRenderer();
+        bitmapFont = new BitmapFont();
+        bitmapFont.setScale(2f);
+        bitmapFont.setColor(0.0f, 0.0f, 0.0f, 1.0f);
+        bitmapFont.getRegion().getTexture().setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
 
         setPosition(0, 450);
         setSize(533, 222);
@@ -25,5 +32,7 @@ public class Description extends Actor {
         shapeRenderer.end();
 
         batch.begin();
+
+        bitmapFont.draw(batch, "Description", 100, 600);
     }
 }
