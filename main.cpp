b270bf190b395162186503c74f5d917e6fa98fbd
Vladimir Zarypov <krre31@gmail.com>
2013-12-08 09:20:13 +0200

    на Андроиде приложение открывается на весь экран, на десктопе - в окне

--- main.cpp
+++ main.cpp
@@ -7,7 +7,12 @@ int main(int argc, char *argv[])
 
     QtQuick2ApplicationViewer viewer;
     viewer.setMainQmlFile(QStringLiteral("qml/Cinema/main.qml"));
+
+#ifdef Q_OS_WIN32
     viewer.showExpanded();
+#else
+    viewer.showFullScreen();
+#endif
 
     return app.exec();
 }
