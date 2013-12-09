82e2cd375ce923de83eca574cd8dcdf26a7fc0c6
Vladimir Zarypov <krre31@gmail.com>
2013-12-09 15:08:16 +0200

    полноэкранный режим только на Андроиде

--- main.cpp
+++ main.cpp
@@ -8,10 +8,10 @@ int main(int argc, char *argv[])
     QtQuick2ApplicationViewer viewer;
     viewer.setSource(QUrl("qrc:/qml/main.qml"));
 
-#ifdef Q_OS_WIN32
-    viewer.showExpanded();
-#else
+#ifdef Q_OS_ANDROID
     viewer.showFullScreen();
+#else
+    viewer.showExpanded();
 #endif
 
     return app.exec();
